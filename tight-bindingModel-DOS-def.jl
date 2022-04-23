552446a30430f816a1319966bcaf444f287b9aeb
ishida_hirone <sakai.39186@gmail.com>
2022-04-23 15:45:05 +0900

    first

---
+++ tight-bindingModel-DOS-def.jl
@@ -0,0 +1,41 @@
+#Ek=-2tcos(ka)を使って状態密度を定義から計算する．D(E)=\sum_{k}\delta(E-Ek)
+#=
+t=1
+a=1
+=#
+
+
+using Plots
+
+
+N=10000 #kの刻み
+M=10000 #epsilonの刻み
+
+function EK(k)
+    return -2*cos(k)
+end
+
+k=range(-π,π,length=N)
+Ek=EK.(k)
+
+
+function count_bins(xmin,xmax,n,data)
+    d=range(xmin,xmax,length=n)
+    count=zeros(Int64,n)
+
+    Delta=(xmax-xmin+1)/n
+
+    for i in data
+        e=convert(Int64,ceil((i-xmin)/Delta))
+        count[e+1] += 1
+    end
+
+    return count
+end
+
+#println(count_bins(-2,2,M,Ek))
+
+d=range(-2,2,length=N)
+
+plot(d,count_bins(-2,2,M,Ek)/(4/M))
+savefig("DOS-def")
---
+++ tight-bindingModel-cal.jl
@@ -0,0 +1,14 @@
+#tight-binding model の状態密度を，計算した表式から計算する．
+
+using Plots
+
+function D(E)
+    return (1/π)*(1/sqrt(4-E*E))
+end
+
+n=1000
+Es = range(-2,2,length=n)
+Ds = D.(Es)
+
+plot(Es,Ds)
+savefig("DOS-cal")
