d4acb47909d53598e3f8d9dea222ff68da7db7fe
ishida_hirone <sakai.39186@gmail.com>
2022-04-27 16:21:34 +0900

    hoge

--- tight-bindingModel-DOS-def.jl
+++ tight-bindingModel-DOS-def.jl
@@ -1,30 +1,30 @@
 using Plots
 
-N=1000000 #kの刻み
-M=1000 #epsilonの刻み
+N = 1000000 #kの刻み
+M = 1000 #epsilonの刻み
 
 function EK(k)
     return -2*cos(k)
 end
 
-k=range(-π,π,length=N)
-Ek=EK.(k)
+k = range(-π, π, length=N)
+Ek = EK.(k)
 
-function count_bins(xmin,xmax,n,data)
-    d=range(xmin,xmax,length=n)
-    count=zeros(Int64,n)
+function count_bins(xmin, xmax, n, data)
+    d = range(xmin, xmax, length=n)
+    count = zeros(Int64, n)
 
-    Delta=(xmax-xmin)/n
+    Delta = (xmax-xmin)/n
 
     for i in data
-        e=convert(Int64,ceil((i-xmin)/Delta))
+        e = convert(Int64,ceil((i-xmin)/Delta))
         count[e] += 1
     end
 
     return count
 end
 
-d=range(-3,3,length=M)
+d = range(-3, 3, length=M)
 
-scatter(d,count_bins(-3,3,M,Ek),markersize=2)
+scatter(d, count_bins(-3,3,M,Ek), markersize=2)
 savefig("DOS-def")
